65240246a738b304afdaf0ced5e478f62dc4ab23
Helen Sparrow <helensp@umich.edu>
2021-12-05 21:50:24 -0500

    help files

--- R/spearmantest.R
+++ R/spearmantest.R
@@ -1,12 +1,29 @@
-#' Add together two numbers
+#' Calculates Spearman's correlation, which is the same as calculating Pearson correlation on the ranked data.
 #'
-#' @param x A number
-#' @param y A number
-#' @return The sum of \code{x} and \code{y}
+#' @param X A vector of numbers
+#' @param Y A vector of numbers
+#' @return The spearman correlation between \code{X} and \code{Y}
 #' @examples
 #' add(1, 1)
 #' add(10, 1)
 #'
+
+spearmantest <- function(X, Y) {
+  Si <- rank(data$dpp)
+  Ri <- rank(data$pct)
+
+  rs <- 1 - (6*sum((Si-Ri)^2))/(n*(n^2-1))
+
+  rsalpha <- qSpearman(p = 0.95, r = n)
+
+  # P-Value
+  pval <- (1-pSpearman(rs, n))
+
+  results <- data.frame('Quantity' = c("Test Statistic (rs)","Critical Value", "P-Value"),
+                        'By-Hand' = c(rs, rsalpha, pval),
+                        'Built-In Func' = c(rsa$estimate, NA, rsa$p.value))
+  return(results)
+}
 
 spearmantest.exact <- function() {
   Si <- rank(data$dpp)
@@ -24,5 +41,3 @@ spearmantest.exact <- function() {
                         'Built-In Func' = c(rsa$estimate, NA, rsa$p.value))
   return(results)
 }
-
-
