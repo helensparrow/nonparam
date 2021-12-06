042a119b4b011c18b7deaa584650d88662963c72
Helen Sparrow <helensp@umich.edu>
2021-12-05 22:55:54 -0500

    test

--- R/spearmantest.R
+++ R/spearmantest.R
@@ -8,6 +8,8 @@
 #' spearmantest(10, 1)
 #'
 
+library(SuppDists)
+
 spearmantest <- function(X, Y) {
   n <- length(X)
 
@@ -21,24 +23,7 @@ spearmantest <- function(X, Y) {
   # P-Value
   pval <- (1-pSpearman(rs, n))
 
-  results <- data.frame('Quantity' = c("Test Statistic (rs)","Critical Value", "P-Value"),
+  results <- data.frame('Quantity' = c("Test Statistic","Critical Value", "P-Value"),
                         'Value' = c(rs, rsalpha, pval))
   return(results)
 }
-
-# spearmantest.exact <- function() {
-#   Si <- rank(data$dpp)
-#   Ri <- rank(data$pct)
-#
-#   rs <- 1 - (6*sum((Si-Ri)^2))/(n*(n^2-1))
-#
-#   rsalpha <- qSpearman(p = 0.95, r = n)
-#
-#   # P-Value
-#   pval <- (1-pSpearman(rs, n))
-#
-#   results <- data.frame('Quantity' = c("Test Statistic (rs)","Critical Value", "P-Value"),
-#                         'By-Hand' = c(rs, rsalpha, pval),
-#                         'Built-In Func' = c(rsa$estimate, NA, rsa$p.value))
-#   return(results)
-# }
